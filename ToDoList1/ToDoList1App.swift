feb5a7bf81fde9c3d6aec37e2736450c5371fd94
morisum <moris@r.sannet.ne.jp>
2023-08-18 17:30:58 +0900

    Initial Commit

---
+++ ToDoList1/ToDoList1App.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoList1App.swift
+//  ToDoList1
+//
+//  Created by MsMacM on 2023/08/18.
+//
+
+import SwiftUI
+
+@main
+struct ToDoList1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
